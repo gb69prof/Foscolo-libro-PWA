c3365a45a84ad184f01d46999e5be65826e2f814
gb69prof <gb69prof@users.noreply.github.com>
2026-04-26 00:30:40 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='foscolo-blocco1-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/originali/Copertina.PNG','./assets/originali/1.PNG','./assets/originali/2.PNG','./assets/originali/3.PNG','./assets/originali/4.PNG','./assets/originali/5.PNG'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
